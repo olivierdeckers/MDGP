0b6b91fe03999d871af3c739bf0d77028a4435e1
Olivier Deckers <deckers.olivier@gmail.com>
2014-03-07 10:30:14 +0100

    initial commit

---
+++ src/MDGP.scala
@@ -0,0 +1,37 @@
+
+class MDGP(file:String) {
+
+  val (nbElements, nbGroups, limits, distances) = initFromFile(file)
+
+  def initFromFile(file:String) = {
+    val (firstLine:String) :: distanceLines = io.Source.fromFile(file).getLines().toList
+    val nbElements :: nbGroups :: groupLimits = firstLine.split(" ").map(s => toInt(s)).flatten.toList
+
+    def readLimits(list:List[Int]) : List[(Int,Int)] = list match {
+      case a :: b :: rest => (a,b) :: readLimits(rest)
+      case _ => List()
+    }
+    val limits = readLimits(groupLimits)
+
+    val distances = Array.ofDim[Int](nbElements, nbElements)
+    distanceLines.foreach(distance => {
+      val Array(a, b, d) = distance.split(" ").map(s => s.toInt)
+      distances(a)(b) = d
+    })
+
+    (nbElements, nbGroups, limits, distances)
+  }
+
+  def toInt(s: String):Option[Int] = {
+    try {
+      Some(s.toInt)
+    } catch {
+      case e:Exception => None
+    }
+  }
+
+}
+
+object Main extends App {
+  new MDGP("RanInt_n010_ds_01.txt")
+}
