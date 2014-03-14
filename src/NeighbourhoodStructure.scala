70c9909ddeb079932233bdcfe86e6020750e60bb
Olivier Deckers <deckers.olivier@gmail.com>
2014-03-14 10:03:52 +0100

    Added interface for neighbourhood structures

---
+++ src/NeighbourhoodStructure.scala
@@ -0,0 +1,16 @@
+import _root_.MDGPSolution.Solution
+
+object NeighbourhoodStructure {
+
+  def insertion(sol:Solution, mdgp:MDGP) = {
+
+  }
+
+  def swap(sol:Solution, mdgp:MDGP) = {
+
+  }
+
+  def threeChain(sol:Solution, mdgp:MDGP) = {
+
+  }
+}
